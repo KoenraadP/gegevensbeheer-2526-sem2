1bcd5029a10b2efa3c876031d201bab5ab21fe4f
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-02-12 10:57:20 +0100

    12021057

--- Les03/scripts/submitted.js
+++ Les03/scripts/submitted.js
@@ -19,12 +19,28 @@ console.log(splitData);
 
 // voor ieder item uit de splitData array
 // een alinea (<p>) maken en toevoegen aan de resultsDiv
-splitData.forEach(element => {
+// splitData.forEach(element => {
+//     // <p> tag aanmaken
+//     let paragraph = document.createElement("p");
+//     // p invullen met waarde van element
+//     // een element is bijvoorbeeld name=Koenraad
+//     paragraph.textContent = element;
+//     // paragraph toevoegen aan resultsDiv
+//     resultsDiv.append(paragraph);
+// });
+
+// alternatieve (betere) manier om aan form data uit URL te raken
+let formData = new URLSearchParams(location.search);
+console.log(formData);
+
+// key --> naam van input
+// value --> waarde van input
+formData.forEach((value,key) => {
     // <p> tag aanmaken
     let paragraph = document.createElement("p");
     // p invullen met waarde van element
     // een element is bijvoorbeeld name=Koenraad
-    paragraph.textContent = element;
+    paragraph.textContent = key + " = " + value;
     // paragraph toevoegen aan resultsDiv
     resultsDiv.append(paragraph);
 });
