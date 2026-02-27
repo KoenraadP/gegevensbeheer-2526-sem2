27d39503bb9d6382b7bae06b5bdf2996dd1b76f9
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-02-27 09:01:40 +0100

    2702901

--- Les05/startbestanden_cookies_localstorage/scripts/language.js
+++ Les05/startbestanden_cookies_localstorage/scripts/language.js
@@ -1,10 +1,13 @@
 "use strict";
 
-// html elementen koppelen
-let frenchLink = document.getElementById("fr");
+// gekozen taal ophalen uit localStorage
+// als er geen key gevonden wordt, is de waarde null
+let chosenLanguage = localStorage.getItem("chosenLanguage"); // zal ofwel null, "nl", "fr" of "en" zijn
+
+// greeting <p> koppelen
 let greetingPar = document.getElementById("greeting");
 
-// alle links verzamelen die gebruikt worden voor taal instellen
+// alle links <a> verzamelen die gebruikt worden voor taal instellen
 let languageLinks = document.querySelectorAll(".language-link");
 
 // click event voor alle languageLinks
@@ -16,12 +19,3 @@ languageLinks.forEach(element => {
         localStorage.setItem("chosenLanguage",e.target.id);
     })
 });
-
-// als ik klik op frenchLink dan wordt de tekst veranderd
-// frenchLink.addEventListener("click", function(e) {
-    // standaard gedrag voorkomen --> niet de pagina herladen
-    // e.preventDefault();
-    // greetingPar.textContent = "Bonjour!";
-    // opslaan in localstorage dat ik voor "fr" gekozen heb
-    // localStorage.setItem("chosenLanguage","fr");
-// })
